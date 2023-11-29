b127a953a5d2a1747a8e22136c4b287c2c0dabd6
sebastianromero07 <sebastian.romero@utec.edu.pe>
2023-11-29 13:46:44 -0600

    Update

--- P2.py
+++ P2.py
@@ -1,7 +1,3 @@
-import numpy as np
-from PIL import Image
-from sklearn.datasets import load_digits
-from scipy.spatial.distance import cdist
 import cv2
 import numpy as np
 from sklearn import datasets
@@ -39,7 +35,92 @@ img_procesada = proceso(image)
 print("\nImagen procesada : \n")
 print(img_procesada)
 
-'''
+# ----------------------
+#   ITEM D
+# ----------------------
+
+def euclidean_distance(img1, img2):
+    # Calcular la distancia euclidiana entre dos imágenes
+    return np.sqrt(np.sum((img1 - img2) ** 2))
+
+def find_most_similar_digits(new_digit, dataset_digits):
+    # Inicializar una lista para almacenar las distancias y los índices de los dígitos
+    distances = []
+    new_flat = new_digit.flatten()
+
+    # Calcular la distancia euclidiana para cada dígito en el dataset
+    for i, digit in enumerate(dataset_digits):
+        distance = euclidean_distance(new_flat, digit)
+        distances.append((distance, i))
+
+    # Ordenar la lista de distancias de menor a mayor
+    distances.sort()
+
+    # Obtener los índices de los 3 dígitos más cercanos
+    closest_indices = [index for _, index in distances[:3]]
+
+    return closest_indices
+
+closest_indices = find_most_similar_digits(img_procesada, dataset_digits)
+
+
+# ----------------------
+#   ITEM E
+# ----------------------
+
+print("\nLos 3 dígitos más parecidos del dataset digits:")
+for i, index in enumerate(closest_indices):
+    print(f"Índice {i + 1}: {index}, Etiqueta: {digitos.target[index]}")
+    print(digitos.images[index].reshape((8, 8)))  # Mostrar la imagen
+    print("-" * 40)
+
+
+
+
+
+
+
+
+
+
+'''import numpy as np
+from PIL import Image
+from sklearn.datasets import load_digits
+from scipy.spatial.distance import cdist
+
+# Función para convertir valores entre 0 y 255 a valores entre 0 y 16
+def convert_values(image):
+    return (16 / 255) * image
+
+# Cargar el conjunto de datos digits
+digits = load_digits()
+
+# Obtener las imágenes y las etiquetas
+images = digits.images
+labels = digits.target
+
+# Ruta de la imagen con el número dibujado
+image_path = 'img/7.jpg'  # Cambia esto a la ruta de tu imagen
+
+# Cargar la imagen y convertirla a escala de grises
+image = Image.open(image_path).convert('L')
+
+# Redimensionar la imagen a 8x8
+image_resized = image.resize((8, 8))
+
+# Convertir la imagen a una matriz de píxeles
+pixel_matrix = np.array(image_resized)
+
+# Convertir valores entre 0 y 255 a valores entre 0 y 16
+pixel_matrix = convert_values(pixel_matrix)
+
+# Normalizar las matrices de píxeles del nuevo dígito y de los dígitos en el conjunto de datos
+pixel_matrix_normalized = pixel_matrix.flatten() / 16.0
+images_normalized = images.reshape(len(images), -1) / 16.0
+
+print("Matriz de píxeles normalizada del nuevo dígito:")
+print(pixel_matrix_normalized)
+
 
 # Calcular las distancias euclidianas utilizando cdist
 distances = cdist(pixel_matrix_normalized.reshape(1, -1), images_normalized, metric='euclidean')
