df3fdf2b8bafc3485b4ee6530d20dc8004e80c5b
estebanSulcaInfante <esteban.sulca@utec.edu.pe>
2023-11-30 02:53:03 -0500

    Ending minimum requirements

--- P2.py
+++ P2.py
@@ -2,6 +2,68 @@ import cv2
 import numpy as np
 from sklearn import datasets
 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+
+
+# Cargar el conjunto de datos digits
+digitos = load_digits()
+
+# Obtener las imágenes y las etiquetas
+images = digitos.images
+labels = digitos.target
+
+# Crear una matriz para almacenar las imágenes promedio
+average_images = np.zeros((10, 8, 8))
+
+# Calcular la imagen promedio para cada dígito
+for i in range(10):
+    digit_images = images[labels == i]
+    average_images[i] = np.mean(digit_images, axis=0)
+
+
+# Función para mostrar la imagen promedio de un dígito
+def mostrar_average_image(digito):
+    plt.imshow(average_images[digito], cmap='gray')
+    plt.title(f'Imagen promedio del dígito {digito}')
+    plt.show()
+
+# Menú para el usuario
+while True:
+    print("1. Mostrar imágenes promedio de todos los dígitos.")
+    print("2. Mostrar imagen promedio de un dígito específico.")
+    print("3. Salir")
+
+    choice = input("Ingrese su elección (1, 2 o 3): ")
+
+    if choice == '1':
+        # Mostrar imágenes promedio de todos los dígitos
+        fig, axs = plt.subplots(2, 5, figsize=(10,5))
+
+        for digit in range(10):
+            row = digit // 5
+            col = digit % 5
+            ax = axs[row, col]
+            im = ax.imshow(average_images[digit], cmap='gray', vmin=0, vmax=16)
+            ax.set_title(f'Promedio #{digit}')
+        cbar = fig.colorbar(im, ax=axs, orientation='vertical', ticks=np.arange(0, 17, 1))
+        cbar.set_label('Valor')
+        plt.show()
+
+    elif choice == '2':
+        # Solicitar al usuario el dígito específico
+        digito = int(input("Ingrese el dígito (0-9): "))
+        if 0 <= digito <= 9:
+            # Mostrar la imagen promedio del dígito especificado
+            mostrar_average_image(digito)
+        else:
+            print("Entrada inválida. Por favor, ingrese un número entre 0 y 9.")
+    elif choice == '3':
+        break
+    else:
+        print("Entrada inválida. Por favor, ingrese 1, 2 o 3.")
+
 # ----------------------
 #   ITEM C
 # ----------------------
@@ -59,9 +121,9 @@ def find_most_similar_digits(new_digit, dataset_digits):
     # Obtener los índices de los 3 dígitos más cercanos
     closest_indices = [index for _, index in distances[:3]]
 
-    return closest_indices
+    return closest_indices, [distance for distance, _ in distances[:3]]
 
-closest_indices = find_most_similar_digits(img_procesada, dataset_digits)
+closest_indices, closest_distances = find_most_similar_digits(img_procesada, dataset_digits)
 
 
 # ----------------------
@@ -75,7 +137,66 @@ for i, index in enumerate(closest_indices):
     print("-" * 40)
 
 
+# ----------------------
+#   ITEM F
+# ----------------------
 
+# diccionario para almacenar las frecuencias de los digitos
+digit_frequencies = {}
+
+# contar y actualizar frecuencias de los digitos
+for idx in closest_indices:
+    label = labels[idx]
+    if label in digit_frequencies:
+        digit_frequencies[label] += 1
+    else:
+        digit_frequencies[label] = 1
+
+# buscar el que se repite mas
+most_common = 0
+max_count = 0
+for label, count in digit_frequencies.items():
+    if count > max_count:
+        most_common = label
+        max_count = count
+      
+#si 2 o 3 son iguales
+if max_count >= 2: 
+    print("\n Soy la inteligencia artificial, y he detectado que el dígito ingresado corresponde al número" , most_common)
+else:
+  # Busca el que tenga la distancia eclidiana mas pequenia
+    min_distance = min(closest_distances)
+    min_distance_index = closest_distances.index(min_distance)
+    closest_digit = labels[closest_indices[min_distance_index]]
+    print("\n Soy la inteligencia artificial, y he detectado que el dígito ingresado corresponde al número", closest_digit)
+
+#imprimir
+closest_digit = labels[min_distance_index]
+print("\n Soy la inteligencia artificial, y he detectado que el dígito ingresado corresponde al número", closest_digit)
+      
+# ----------------------
+#   ITEM G
+# ----------------------
+    
+          
+def calcular_distancia_a_promedios(new_digit, average_images):
+    min_distance = float('inf')
+    closest_average_digit = None
+
+    for i in range(10):
+        distance = euclidean_distance(new_digit.flatten(), average_images[i].flatten())
+        if distance < min_distance:
+            min_distance = distance
+            closest_average_digit = i
+
+    return closest_average_digit, min_distance
+
+closest_average_digit, min_distance_to_average = calcular_distancia_a_promedios(img_procesada, average_images)
+print(f"\nSoy la inteligencia artificial versión 2, y he
+detectado que el dígito ingresado corresponde al número {closest_average_digit}”, donde {closest_average_digit} es 
+un número entre 0 y 9.")
+
+            
 
 
 
